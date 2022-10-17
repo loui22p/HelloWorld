15862fc2e62dc25a32d404014757a9b980564ce8
Mikkel <miopedersen@gmail.com>
2022-10-17 19:05:29 +0200

    Created initial Hello World project.

---
+++ MikkelTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MikkelTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
